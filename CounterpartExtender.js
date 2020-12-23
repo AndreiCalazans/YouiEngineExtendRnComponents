58258897452c0e6550dd002b03e4f495eddf33f9
Andrei Calazans <andreixoc@hotmail.com>
2020-12-23 19:00:46 -0300

    Listen to events

--- CounterpartExtender.js
+++ CounterpartExtender.js
@@ -1,15 +1,51 @@
 import React, { useRef, forwardRef, useImperativeHandle } from 'react';
-import { NativeModules, findNodeHandle } from 'react-native';
+import { NativeModules, findNodeHandle, NativeEventEmitter } from 'react-native';
 
-export const CounterpartExtender = forwardRef(({ component, ...remainingProps }, ref) => {
+const CounterpartExtensionEmitter = new NativeEventEmitter(
+    NativeModules.CounterpartExtension
+);
+
+export const CounterpartExtender = forwardRef(({
+  component,
+  onListLostFocus,
+  onListGainedFocus,
+  onFocusChanged,
+  ...remainingProps
+}, ref) => {
   const compRef = useRef(null);
   // Send the ref back up to parent
   useImperativeHandle(ref, () => compRef.current);
   const Comp = component;
 
     useEffect(() => {
-        const listTag = findNodeHandle(listRef.current);
-        NativeModules.CounterpartExtension.extendCounterpart(listTag);
+      const listTag = findNodeHandle(listRef.current);
+      NativeModules.CounterpartExtension.extendCounterpart(listTag);
+
+      const listeners = [];
+
+      if (onListLostFocus) {
+        listeners.push(CounterpartExtensionEmitter.addListener(
+          "ListLostFocus",
+          onListLostFocus
+        ));
+      }
+
+      if (onListGainedFocus) {
+        listeners.push(CounterpartExtensionEmitter.addListener(
+          "ListGainedFocus",
+          onListGainedFocus
+        ));
+      }
+
+      if (onFocusChanged) {
+        listeners.push(CounterpartExtensionEmitter.addListener(
+          "ListChangedFocus",
+          onFocusChanged
+        ));
+      }
+
+      return () => listeners.forEach(listener => listener.remove());
+      
     }, []);
 
   return <Comp ref={compRef}  {...remainingProps} />
