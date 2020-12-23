223154fcefa0f22d41b6d2620c3f5a89dae18052
Andrei Calazans <andreixoc@hotmail.com>
2020-12-23 14:54:52 -0300

    Call extendCounterpart

--- CounterpartExtender.js
+++ CounterpartExtender.js
@@ -1,9 +1,16 @@
 import React, { useRef, forwardRef, useImperativeHandle } from 'react';
+import { NativeModules, findNodeHandle } from 'react-native';
 
 export const CounterpartExtender = forwardRef(({ component, ...remainingProps }, ref) => {
   const compRef = useRef(null);
   // Send the ref back up to parent
   useImperativeHandle(ref, () => compRef.current);
   const Comp = component;
+
+    useEffect(() => {
+        const listTag = findNodeHandle(listRef.current);
+        NativeModules.CounterpartExtension.extendCounterpart(listTag);
+    }, []);
+
   return <Comp ref={compRef}  {...remainingProps} />
 })
