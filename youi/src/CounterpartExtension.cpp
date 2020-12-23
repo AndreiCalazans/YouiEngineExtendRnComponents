5ef64c08a1cc1ddbcf624af874ec409efad810b2
Andrei Calazans <andreixoc@hotmail.com>
2020-12-23 13:57:38 -0300

    Add implementation file

---
+++ youi/src/CounterpartExtension.cpp
@@ -0,0 +1,28 @@
+#include "CounterpartExtension.h"
+
+#include <youireact/NativeModuleRegistry.h>
+#include <scenetree/YiSceneManager.h>
+
+#include <youireact/IBridge.h>
+#include <youireact/ShadowTree.h>
+
+using namespace folly;
+using namespace std;
+
+const CYIString TAG = "CounterpartExtension";
+
+YI_RN_INSTANTIATE_MODULE(CounterpartExtension);
+YI_RN_REGISTER_MODULE(CounterpartExtension);
+
+
+YI_RN_DEFINE_EXPORT_METHOD(CounterpartExtension, extendCounterpart)(uint64_t tag)
+{
+    // ShadowRegistry contains all of the items available in the ShadowTree (similar to the virtual DOM).
+    auto &shadowRegistry = GetBridge().GetShadowTree().GetShadowRegistry();
+    auto pComponent = shadowRegistry.Get(tag);
+    YI_ASSERT(pComponent, TAG, "Shadow view with tag %" PRIu64 " not found in ShadowRegistry.", tag);
+
+    // For every React Native component we have a corresponding Widget (counterpart) in the Engine. 
+    auto pCounterpart = pComponent->GetCounterpart(); //CYISceneNode 
+    YI_ASSERT(pCounterpart, TAG, "Shadow view with tag %" PRIu64 " doesn't have a counterpart.", tag);
+}
