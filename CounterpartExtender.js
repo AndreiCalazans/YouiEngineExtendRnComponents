01998cad579dbd9e3d9884bb569b934117b2dd70
Andrei Calazans <andreixoc@hotmail.com>
2020-12-23 14:38:22 -0300

    get ref of CounterpartExtender

--- CounterpartExtender.js
+++ CounterpartExtender.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 export const CounterpartExtender = ({ component, ...remainingProps }) => {
+  const compRef = useRef(null);
   const Comp = component;
-  return <Comp  {...remainingProps} />
+  return <Comp ref={compRef}  {...remainingProps} />
 }
